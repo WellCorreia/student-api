f12da9d749e7af9398fcee04ad20c6166507d5bc
Wellington Correia <wellington.dossantos@montreal.com.br>
2021-03-18 10:30:44 -0300

    Developed builder for tests

---
+++ src/test/java/com/wellington/student/builder/StudentDTOBuilder.java
@@ -0,0 +1,28 @@
+package com.wellington.student.builder;
+
+import com.wellington.student.dto.StudentDTO;
+import com.wellington.student.enums.GradeType;
+import lombok.Builder;
+
+@Builder
+public class StudentDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Wellington";
+
+    @Builder.Default
+    private String registry = "3214569877";
+
+    @Builder.Default
+    private int age = 25;
+
+    @Builder.Default
+    private GradeType grade = GradeType.FIRST;
+
+    public StudentDTO toStudentDTO() {
+        return new StudentDTO(id, name, age, registry, grade);
+    }
+}
